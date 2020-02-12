a8bb55752dfc245ba12c801d1d9f23cfc2e7be20
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-02-11 18:27:37 -0600

    ya

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,18 @@
-<script>
+
     function doInputOutput(){
-        let temp = document.getElementById("temp").value;
-        let windspeed = document.getElementById("speed").value;
+        let temp = 73;
+        let windspeed = 5;
         let wc = windChill(temp, windspeed);
-        document.getElementById("outputDiv").innerHTML = "The Wind Chill is" + wc;
+
+        let digits = 0;
+    let multiplier = Math.pow(10, digits);
+    wc = Math.round(wc * multiplier) / multiplier;
+        
+        document.getElementById("outputDiv").innerHTML = "The Wind Chill is" +  wc;
     }
     function windChill(tempF, speed){
         
         let f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
         return f;
-        
+       
     }
-    </script>
