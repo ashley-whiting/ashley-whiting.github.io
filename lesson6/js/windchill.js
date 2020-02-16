2aeb6ff12228db27e0e76f38f132c47fc66227a1
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-02-15 18:02:14 -0600

    temp

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,18 +1,18 @@
 
-    function doInputOutput(){
-        let temp = 73;
-        let windspeed = 5;
-        let wc = windChill(temp, windspeed);
+        let tempF = parseFloat(document.getElementById ("temp").innerHTML);
+        let speed = parseFloat(document.getElementById("wind").innerHTML);
+        
 
-        let digits = 0;
-    let multiplier = Math.pow(10, digits);
-    wc = Math.round(wc * multiplier) / multiplier;
+   
         
-        document.getElementById("outputDiv").innerHTML = "The Wind Chill is" +  wc;
-    }
-    function windChill(tempF, speed){
-        
+    if (temp > 50 && wind < 3) {
+        document.getElementById("windchill").innerHTML = "N/A";}
+        else{    
+            
         let f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
-        return f;
+
+            document.getElementById('windchill').innerHTML = f.toFixed(1);
+       
        
     }
+
