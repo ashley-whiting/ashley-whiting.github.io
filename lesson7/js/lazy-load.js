a60d4af07376a0a934ed227fe2bf7404b92175ae
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-02-19 19:13:30 -0600

    yes

--- lesson7/js/lazy-load.js
+++ lesson7/js/lazy-load.js
@@ -3,25 +3,27 @@ const imagesToLoad = document.querySelectorAll("[data-src]");
 
 // optional parameters being set for the IntersectionalObserver
 const imgOptions = {
-    threshold: 0;
-    rootMargin: "0px 0px 300px 0px"
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {image.removeAttribute('data-src');};
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
 };
 
 // check to see if Intersection Observer is supported
 if('IntersectionObserver' in window) {
-        const imgObserver = new IntersectionObserver((items, observer) =>) {
+        const imgObserver = new IntersectionObserver((items, observer) => {
             items.forEach((item) => {
-                if(item.isIntersecting){
+                if(item.isIntersecting) {
                     loadImages(item.target);
                     observer.unobserve(item.target);
                 }
             });
-        });
+        }, imgOptions);
 
         //Loop through each img and check status and load if necessary
         imagesToLoad.forEach((img) => {
