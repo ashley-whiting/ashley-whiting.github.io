c55f103863115d9f10077fdef954a756e8780510
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-03-04 20:20:09 -0600

    ook

---
+++ lesson9/js/cities.js
@@ -0,0 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+    const towns = jsonObject['towns'];
+    
+    for (let i = 0; i < towns.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('h3');
+        let yearFounded = document.createElement('p');
+        let population = document.createElement('p');
+        let rainfall = document.createElement('p');
+        let image = document.createElement('img');
+
+h2.textContent = towns[i].name;
+motto.textContent = towns[i].motto;
+yearFounded.textContent = "Year Founded:" + towns[i].yearFounded;
+population.textContent = "Current Population" + towns[i].currentPopulation;
+rainfall.textContent = "Average Rainfall:" + towns[i].averageRainfall;
+image.setAttribute('src', towns[i].imageurl);
+image.setAttribute('alt',towns[i].name + (i+1));
+
+card.appendChild(h2);
+card.appendChild(motto);
+card.appendChild(yearFounded);
+card.appendChild(population);
+card.appendChild(rainfall);
+
+document.querySelector('div.cards').appendChild(card);}
+  });
---
+++ lesson9/js/webfont.js
@@ -0,0 +1,8 @@
+
+WebFont.load({
+    google: {
+      families: ['Overpass', sans-serif, 'Raleway', sans-serif]
+    }
+    });
+    
+    
