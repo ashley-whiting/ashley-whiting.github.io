b4ffdefc49fcaf36e164eb4d9e3e5c733a3530c3
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-03-10 12:03:52 -0500

    yes

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,13 +1,13 @@
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=117b7f64ed1dc97009b2e9dcf0623b82';
 fetch(apiURL)
   .then((response) => response.json())
-  .then((apiObject) => {
-    console.log(apiObject);
-    document.getElementById('current-temp').textContent = apiObject.main.temp;
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
 
  
-  const imagesrc = 'https://openweathermap.org/img/w/' + apiObject.weather[0].icon + '.png';  // note the concatenation
-  const desc = apiObject.weather[0].description;  // note how we reference the weather array
+  const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+  const desc = jsObject.weather[0].description;  // note how we reference the weather array
   
   document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
   document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
