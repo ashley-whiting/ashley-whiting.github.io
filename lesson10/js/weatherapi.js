766ccdc34ad105be928da6a4531fb67852942974
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-03-13 19:05:17 -0500

    ok

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=117b7f64ed1dc97009b2e9dcf0623b82';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=117b7f64ed1dc97009b2e9dcf0623b82';
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
