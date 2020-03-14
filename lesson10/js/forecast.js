e320ef94a517213c0bd8579db7e408dd46dc554d
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-03-13 23:36:22 -0500

    ok

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,6 +1,23 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=117b7f64ed1dc97009b2e9dcf0623b82';
 
-fetch(apiURL)
+    let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = new Date();
+    const today = day.getDay();
+    let i;
+    
+    for (i=1; i<6; i++){
+        if ((today + i) <7){
+            document.getElementById('day' + i).innherHTML = dayName[today + i];
+            }
+            else{
+                document.getElementById('day' + i).innerHTML = dayName[(today + i) - 7];
+            }
+    }
+
+
+
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=117b7f64ed1dc97009b2e9dcf0623b82';
+
+fetch(forecastURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
@@ -12,6 +29,22 @@ fetch(apiURL)
     console.log(fivedayforecast);
 
     for (let i=0; i<fivedayforecast.length; i++){
-        document.getElementById('forecast${i+1}' ).textContent = fivedayforecast[i].main.temp;
+        document.getElementById('forecast' + [i+1]).
+        textContent = fivedayforecast[i].main.temp;
     }
+
+
+    let imgsrc;
+    let des;
+
+    for (let i = 0; i<fivedayforecast.length; i++){
+        imgsrc = 'https://openweathermap.org/img/w/' + fivedayforecast[i].weather[0].icon +'.png';
+        desc = fivedayforecast[i].weather[0].description; 
+    
+    document.getElementById('icon' + [i+1]).textContent = imgsrc;  // informational specification only
+    document.getElementById('icon' + [i+ 1]).setAttribute('src', imgsrc);  // focus on the setAttribute() method
+    document.getElementById('icon' + [i + 1]).setAttribute('alt', des);
+  }
+
+
 });
