79b1e2572e8754c1db69a48bd84947766d70b741
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-03-13 19:29:37 -0500

    js

---
+++ lesson10/js/forecast.js
@@ -0,0 +1,8 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=117b7f64ed1dc97009b2e9dcf0623b82';
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+});
