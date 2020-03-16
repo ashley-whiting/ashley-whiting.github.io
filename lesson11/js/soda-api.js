ebad244e3abed1a54f534255b144adc9794c04ab
ashley-whiting <59624107+ashley-whiting@users.noreply.github.com>
2020-03-16 15:17:50 -0500

    js

---
+++ lesson11/js/soda-api.js
@@ -0,0 +1,31 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=117b7f64ed1dc97009b2e9dcf0623b82';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current').textContent = jsObject.weather[0].main;
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('high').textContent = jsObject.main.temp;
+    document.getElementById('speed').textContent = jsObject.wind.speed;
+    let tempF = parseFloat(document.getElementById ("high").innerHTML);
+
+
+        let speed = parseFloat(document.getElementById("speed").innerHTML);
+        
+
+   
+        
+    if (tempF > 50 && speed < 3) {
+        document.getElementById("windchill").innerHTML = "N/A";}
+        else{    
+            
+        let f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
+
+            document.getElementById('windchill').innerHTML = f.toFixed(1);
+       
+       
+    }
+
+    
+
+});
